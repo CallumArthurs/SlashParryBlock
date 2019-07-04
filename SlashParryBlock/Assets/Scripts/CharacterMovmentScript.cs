ae45b2a5184024b16a7d6aff14246ddf00039272
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2019-07-04 15:20:02 +1000

    Update
    +Added a controller script to take inputs from up to four controllers
    +Added Input Axis HorizontalP1-4 & VerticalP1-4

---
+++ SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovmentScript : MonoBehaviour
+{
+    public float speed;
+    public GameObject[] Players;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < Players.Length; i++)
+        {
+            Players[i].transform.Translate(new Vector3(Input.GetAxis("HorizontalP" + (i + 1)) * Time.deltaTime * speed, 0, Input.GetAxis("VerticalP" + (i + 1)) * Time.deltaTime * speed));
+        }
+    }
+}
