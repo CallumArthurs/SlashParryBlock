ec151609890db2ecde8b533556e3a31b89bfd809
JaredInchley <51223370+JaredInchley@users.noreply.github.com>
2019-07-04 16:57:46 +1000

    Level

--- SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
+++ SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
@@ -15,7 +15,7 @@ public class CharacterMovmentScript : MonoBehaviour
     {
         for(int i = 0; i < Players.Length; i++)
         {
-            Players[i].transform.Translate(new Vector3(Input.GetAxis("HorizontalP" + (i + 1)) * Time.deltaTime * speed, 0, Input.GetAxis("VerticalP" + (i + 1)) * Time.deltaTime * speed));
+            Players[i].transform.Translate(new Vector3(Input.GetAxis("HorizontalP" + (i + 1)) * Time.deltaTime * speed, 0, -Input.GetAxis("VerticalP" + (i + 1)) * Time.deltaTime * speed));
         }
     }
 }
