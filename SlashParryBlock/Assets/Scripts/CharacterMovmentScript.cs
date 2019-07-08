bfd831d4aaaa4f376c2c7241d75d91cfe412e05e
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2019-07-08 15:52:32 +1000

    Update
    +Players now can rotate with right analog stick
    +Players can now do a placeholder attack waiting on animation
    +Players can now block attacks that they are facing
    +Players do extra damage on a backstab
    +Players have health and damage data attached to them

--- SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
+++ SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CharacterMovmentScript : MonoBehaviour
 {
     public float speed;
-    public GameObject[] Players;
+    public float rotSpeed;
+    public Rigidbody[] Players;
     void Start()
     {
         
@@ -15,7 +16,27 @@ public class CharacterMovmentScript : MonoBehaviour
     {
         for(int i = 0; i < Players.Length; i++)
         {
-            Players[i].transform.Translate(new Vector3(Input.GetAxis("HorizontalP" + (i + 1)) * Time.deltaTime * speed, 0, -Input.GetAxis("VerticalP" + (i + 1)) * Time.deltaTime * speed));
+            if (Input.GetAxis("HorizontalP" + (i + 1)) != 0 || Input.GetAxis("VerticalP" + (i + 1)) != 0)
+            {
+                Players[i].MovePosition(Players[i].position + new Vector3(Input.GetAxis("HorizontalP" + (i + 1)) * Time.deltaTime * speed, 0, -Input.GetAxis("VerticalP" + (i + 1)) * Time.deltaTime * speed));
+            }
+            
+            if (Input.GetAxis("R_StickHorizontalP" + (i + 1)) != 0 || Input.GetAxis("R_StickVerticalP" + (i + 1)) != 0)
+            {
+                Players[i].rotation = Quaternion.RotateTowards(Players[i].rotation, Quaternion.LookRotation(new Vector3(Input.GetAxis("R_StickHorizontalP" + (i + 1)), 0, -Input.GetAxis("R_StickVerticalP" + (i + 1))), Vector3.up), rotSpeed);
+            }
+
+            if (Input.GetAxis("L_BumperP" + (i + 1)) > 0){
+                Players[i].GetComponent<PlayerData>().Blocking = true;
+            }
+            else
+            {
+                Players[i].GetComponent<PlayerData>().Blocking = false;
+                if (Input.GetAxis("R_BumperP" + (i + 1)) > 0)
+                {
+                    Players[i].gameObject.GetComponent<PlayerData>().Attack();
+                }
+            }
         }
     }
 }
---
+++ SlashParryBlock/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public int health;
+    public int Damage;
+    public bool Blocking;
+    private BoxCollider swordCollider;
+    private float AttackTimer = 0.5f;
+    private bool attacked = false;
+    void Start()
+    {
+        swordCollider = gameObject.GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log(gameObject.name + " is dead");
+        }
+
+        if (Blocking)
+        {
+            Debug.Log(gameObject.name + " am blocking");
+        }
+
+        if (attacked)
+        {
+            AttackTimer -= Time.deltaTime;
+            if (AttackTimer <= 0)
+            {
+                swordCollider.enabled = false;
+                attacked = false;
+                AttackTimer = 0.5f;
+                
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        if (attacked)
+        {
+            return;
+        }
+        swordCollider.enabled = true;
+        attacked = true;
+    }
+    
+    void TakeDamage(int damage)
+    {
+        Debug.Log(gameObject.name + " am hit");
+        health -= damage;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (attacked)
+        {
+            swordCollider.enabled = false;
+            if (other.GetComponent<PlayerData>() != null && other.GetComponent<PlayerData>() != this)
+            {
+                if (other.GetComponent<PlayerData>().Blocking)
+                {
+                    if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.0f)
+                    {
+                        if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.7f)
+                        {
+                            other.GetComponent<PlayerData>().TakeDamage(Damage * 2);
+                            Debug.Log(gameObject.name + " get Backstabbed");
+                            return;
+                        }
+                        other.GetComponent<PlayerData>().TakeDamage(Damage);
+                    }
+                    else
+                    {
+                        Debug.Log(gameObject.name + " get blocked");
+                    }
+                }
+                else
+                {
+                    if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.7f)
+                    {
+                        other.GetComponent<PlayerData>().TakeDamage(Damage * 2);
+                        Debug.Log(gameObject.name + " get Backstabbed");
+                    }
+                    else
+                    {
+                        other.GetComponent<PlayerData>().TakeDamage(Damage);
+                    }
+                }
+            }
+        }
+    }
+}
