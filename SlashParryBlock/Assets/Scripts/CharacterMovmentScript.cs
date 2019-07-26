5adb867e0f1f5e05eebbc974bddea9eee274c8b5
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2019-07-26 13:36:15 +1000

    Update
    + random spawn points
    + parry and riposte mechanics

--- SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
+++ SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
@@ -5,70 +5,87 @@ using UnityEngine.UI;
 
 public class CharacterMovmentScript : MonoBehaviour
 {
+    //this sets the default values for the players at game start
     [Header("Player data default values")]
     public int playerHealth;
-    public int playerDamage;
-    [Tooltip("this is a multiplyer on knockback hit halved for the shield")]
+    public int playerDamage,backstabDamage,riposteDamage;
+    [Tooltip("this is a multiplier on knockback hit, halved for the shield")]
     public float playerKnockback;
 
-    [Header("Character movement values")]
+    [Header("CharacterMovementScript values")]
     public float speed;
     public float rotSpeed;
     public PlayerData[] players;
     public Text[] healthText;
     public Text[] scoreText;
 
-    private List<Rigidbody> playersRB;
-    private List<Vector3> SpawnPoints;
+    private List<Rigidbody> playersRB = new List<Rigidbody>();
     void Start()
     {
-
-        GameObject[] tempobj = GameObject.FindGameObjectsWithTag("SpawnPoint");
-        SpawnPoints = new List<Vector3>();
-        for (int i = 0; i < tempobj.Length; i++)
-        {
-            SpawnPoints.Add(tempobj[i].transform.position);
-        }
-
-        playersRB = new List<Rigidbody>();
-        for (int i = 0; i < 4; i++)
-        {
-            playersRB.Add(players[i].gameObject.GetComponent<Rigidbody>());
-        }
-
+        //setting the values of the players
         for (int i = 0; i < 4; i++)
         {
             players[i].setHealth(playerHealth);
-            players[i].setDamage(playerDamage);
+            players[i].setDamage(playerDamage, backstabDamage, riposteDamage);
             players[i].setKnockback(playerKnockback);
+        }
+        //getting the spawnpoint parent to find all the spawnpoints
+        Transform[] tempobj = GameObject.FindGameObjectWithTag("SpawnPoints").GetComponentsInChildren<Transform>();
+        //starting at 1 to skip the parent
+        for (int i = 1; i < tempobj.Length; i++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                players[k].spawnpoints.Add(tempobj[i].position);
+            }
+        }
+        //getting a reference to all the player's rigidbodies
+        for (int i = 0; i < 4; i++)
+        {
+            playersRB.Add(players[i].gameObject.GetComponent<Rigidbody>());
         }
     }
 
     void Update()
     {
+        //iterate through all the players
         for(int i = 0; i < playersRB.Count; i++)
         {
-            if (Input.GetAxis("HorizontalP" + (i + 1)) != 0 || Input.GetAxis("VerticalP" + (i + 1)) != 0)
+            if (!players[i].getIsParried())
             {
-                playersRB[i].MovePosition(playersRB[i].position + new Vector3(Input.GetAxis("HorizontalP" + (i + 1)) * Time.deltaTime * speed, 0, -Input.GetAxis("VerticalP" + (i + 1)) * Time.deltaTime * speed));
-            }
-            
-            if (Input.GetAxis("R_StickHorizontalP" + (i + 1)) != 0 || Input.GetAxis("R_StickVerticalP" + (i + 1)) != 0)
-            {
-                playersRB[i].rotation = Quaternion.RotateTowards(playersRB[i].rotation, Quaternion.LookRotation(new Vector3(Input.GetAxis("R_StickHorizontalP" + (i + 1)), 0, -Input.GetAxis("R_StickVerticalP" + (i + 1))), Vector3.up), rotSpeed);
-            }
-
-            if (Input.GetAxis("L_BumperP" + (i + 1)) > 0){
-                players[i].blocking = true;
-            }
-            else
-            {
-                players[i].blocking = false;
-                if (Input.GetAxis("R_BumperP" + (i + 1)) > 0)
+                //for player1 this will evaluate to "HorizontalP1"
+                if (Input.GetAxis("HorizontalP" + (i + 1)) != 0 || Input.GetAxis("VerticalP" + (i + 1)) != 0)
                 {
-                    players[i].Attack();
+                    playersRB[i].MovePosition(playersRB[i].position + new Vector3(Input.GetAxis("HorizontalP" + (i + 1)) * Time.deltaTime * speed, 0, -Input.GetAxis("VerticalP" + (i + 1)) * Time.deltaTime * speed));
                 }
+
+                if (Input.GetAxis("R_StickHorizontalP" + (i + 1)) != 0 || Input.GetAxis("R_StickVerticalP" + (i + 1)) != 0)
+                {
+                    playersRB[i].rotation = Quaternion.RotateTowards(playersRB[i].rotation, Quaternion.LookRotation(new Vector3(Input.GetAxis("R_StickHorizontalP" + (i + 1)), 0, -Input.GetAxis("R_StickVerticalP" + (i + 1))), Vector3.up), rotSpeed);
+                }
+
+                if (Input.GetAxis("L_BumperP" + (i + 1)) > 0 && !players[i].getParried())
+                {
+                    players[i].blocking = true;
+                }
+                else
+                {
+                    players[i].blocking = false;
+                    if (Input.GetAxis("R_BumperP" + (i + 1)) > 0 && !players[i].getParried())
+                    {
+                        players[i].Attack();
+                    }
+                    else
+                    {
+                        if (Input.GetAxis("L_TriggerP" + (i + 1)) < 0)
+                        {
+                            players[i].Parry();
+                        }
+                    }
+                }
+
             }
+            //updating the ui on game loop
             healthText[i].text = players[i].getHealth().ToString();
             scoreText[i].text = players[i].score.ToString();
         }
--- SlashParryBlock/Assets/Scripts/PlayerData.cs
+++ SlashParryBlock/Assets/Scripts/PlayerData.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerData : MonoBehaviour
 {
+    [HideInInspector]
     public int score;
+    [HideInInspector]
     public bool blocking;
+    [HideInInspector]
+    public List<Vector3> spawnpoints = new List<Vector3>();
 
-    private int health;
-    private int damage;
+    //original health is their spawned health 
+    private int originalHealth, health;
+    private int damage, backstabDamage, RiposteDamage;
     private float knockback;
     private BoxCollider swordCollider;
+    //how long an attack goes for this is temp fix waitng for animation
     private float AttackTimer = 0.5f;
-    private bool attacked = false;
+    private float ParryTimer = 0.5f;
+    private float gotParriedTimer = 2.0f;
+    private bool attacked = false, parried = false, isParried = false;
+    
     void Start()
     {
+        //getting the collider that attacks people
         swordCollider = gameObject.GetComponent<BoxCollider>();
     }
 
@@ -22,110 +33,214 @@ public class PlayerData : MonoBehaviour
     {
         if (health <= 0)
         {
-            Debug.Log(gameObject.name + " is dead");
+            Respawn();
+        }
+
+        if (transform.position.y <= -5.0f)
+        {
+            health = 0;
         }
 
         if (blocking)
         {
-            Debug.Log(gameObject.name + " am blocking");
+
         }
 
         if (attacked)
         {
+            //run the timer for the attack
             AttackTimer -= Time.deltaTime;
             if (AttackTimer <= 0)
             {
+                //turn off attack and reset timer
                 swordCollider.enabled = false;
                 attacked = false;
                 AttackTimer = 0.5f;
                 
             }
         }
+
+        if (parried)
+        {
+            //run the timer for the attack
+            ParryTimer -= Time.deltaTime;
+            if (ParryTimer <= 0)
+            {
+                //turn off attack and reset timer
+                swordCollider.enabled = false;
+                parried = false;
+                ParryTimer = 0.5f;
+
+            }
+        }
+
+        if (isParried)
+        {
+            //run the timer for the attack
+            gotParriedTimer -= Time.deltaTime;
+            if (gotParriedTimer <= 0)
+            {
+                isParried = false;
+                gotParriedTimer = 2.0f;
+            }
+        }
     }
 
     public void Attack()
     {
-        if (attacked)
+        //makes sure you can't reset the timer
+        if (attacked || parried)
         {
             return;
         }
         swordCollider.enabled = true;
         attacked = true;
     }
+
+    public void Parry()
+    {
+        attacked = false;
+        AttackTimer = 0.5f;
+        if (parried)
+        {
+            return;
+        }
+        swordCollider.enabled = true;
+        parried = true;
+    }
     
     void TakeDamage(int damage)
     {
-        Debug.Log(gameObject.name + " am hit");
         health -= damage;
     }
 
     public void setHealth(int value)
     {
+        originalHealth = value;
         health = value;
     }
     public int getHealth()
     {
         return (health);
     }
-    public void setDamage(int value)
+    public void setDamage(int value,int backValue,int ripValue)
     {
         damage = value;
+        backstabDamage = backValue;
+        RiposteDamage = ripValue;
     }
     public void setKnockback(float value)
     {
         knockback = value;
     }
+    public bool getParried()
+    {
+        return (parried);
+    }
+    public bool getIsParried()
+    {
+        return (isParried);
+    }
+
+    private void Respawn()
+    {
+        //reset health
+        health = originalHealth;
+        //sleep the rigidbody because velocity doesn't update properly
+        GetComponent<Rigidbody>().Sleep();
+        GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+        GetComponent<Rigidbody>().position = spawnpoints[Random.Range(0, spawnpoints.Count)];
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        //caching these for readability
+        PlayerData CollisionPlayerData = other.GetComponent<PlayerData>();
+        Rigidbody CollisionRigidBody = other.GetComponent<Rigidbody>();
+
         if (attacked)
         {
             swordCollider.enabled = false;
-            if (other.GetComponent<PlayerData>() != null && other.GetComponent<PlayerData>() != this)
+
+            //making sure you acually hit someone, and making sure you didn't hit yourself
+            if (CollisionPlayerData != null && CollisionPlayerData != this)
             {
-                if (other.GetComponent<PlayerData>().blocking)
+                if (CollisionPlayerData.blocking)
                 {
+                    //dot product confirms which direction you hit the other player from
+                    //1 = back
+                    //-1 = front
                     if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.0f)
                     {
-                        if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.7f)
+                        if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.7f) // hit their back with shield up
                         {
-                            other.gameObject.GetComponent<Rigidbody>().AddForce((other.transform.position - transform.position) * (knockback * 0.5f), ForceMode.Impulse);
-                            other.GetComponent<PlayerData>().TakeDamage(damage * 2);
-                            Debug.Log(gameObject.name + " get Backstabbed");
+                            //reset their velocity so it doesn't add up after every hit
+                            CollisionRigidBody.velocity = new Vector3(0, 0, 0);
+                            //knockback isn't halved as you didn't hit their shield
+                            CollisionRigidBody.AddForce((other.transform.position - transform.position) * knockback, ForceMode.Impulse);
+                            //double damage if you hit their back
+                            CollisionPlayerData.TakeDamage(backstabDamage);
                         }
-                        else
+                        else // hit their side with shield up
                         {
-                            other.gameObject.GetComponent<Rigidbody>().AddForce((other.transform.position - transform.position) * (knockback * 0.5f), ForceMode.Impulse);
-                            other.GetComponent<PlayerData>().TakeDamage(damage);
+                            CollisionRigidBody.velocity = new Vector3(0, 0, 0);
+                            //knockback is halved as you hit their shield
+                            CollisionRigidBody.AddForce((other.transform.position - transform.position) * (knockback * 0.5f), ForceMode.Impulse);
+                            CollisionPlayerData.TakeDamage(damage);
                         }
                     }
-                    else
+                    else //hit their shield
                     {
-                        other.gameObject.GetComponent<Rigidbody>().AddForce((other.transform.position - transform.position) * (knockback * 0.5f), ForceMode.Impulse);
-                        Debug.Log(gameObject.name + " get blocked");
+                        CollisionRigidBody.velocity = new Vector3(0, 0, 0);
+                        CollisionRigidBody.AddForce((other.transform.position - transform.position) * (knockback * 0.5f), ForceMode.Impulse);
                     }
                 }
-                else
+                else // no shield
                 {
-                    if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.7f)
+                    //hit their front during riposte
+                    if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) < 0.0f && CollisionPlayerData.isParried)
                     {
-                        other.gameObject.GetComponent<Rigidbody>().AddForce((other.transform.position - transform.position) * knockback, ForceMode.Impulse);
-                        other.GetComponent<PlayerData>().TakeDamage(damage * 2);
-                        Debug.Log(gameObject.name + " get Backstabbed");
+                        CollisionRigidBody.velocity = new Vector3(0, 0, 0);
+                        CollisionRigidBody.AddForce((other.transform.position - transform.position) * knockback, ForceMode.Impulse);
+                        CollisionPlayerData.TakeDamage(RiposteDamage);
+                        CollisionPlayerData.isParried = false;
+                        CollisionPlayerData.gotParriedTimer = 2.0f;
                     }
-                    else
+                    else if (Vector3.Dot(other.GetComponent<Transform>().forward, transform.forward) > 0.7f) // hit their back normal hit
                     {
-                        other.GetComponent<Rigidbody>().AddForce((other.transform.position - transform.position) * knockback, ForceMode.Impulse);
-                        other.GetComponent<PlayerData>().TakeDamage(damage);
+                        CollisionRigidBody.velocity = new Vector3(0, 0, 0);
+                        CollisionRigidBody.AddForce((other.transform.position - transform.position) * knockback, ForceMode.Impulse);
+                        CollisionPlayerData.TakeDamage(backstabDamage);
+                    }
+                    else // hit their side or front
+                    {
+                        CollisionRigidBody.velocity = new Vector3(0, 0, 0);
+                        CollisionRigidBody.AddForce((other.transform.position - transform.position) * knockback, ForceMode.Impulse);
+                        CollisionPlayerData.TakeDamage(damage);
                     }
                 }
             }
-
+            else
+            {
+                return;
+            }
+            //check if you killed them and add points if so
             if (other.GetComponent<PlayerData>().health <= 0)
             {
                 score++;
             }
+        }
+        else if (parried)
+        {
+            if (CollisionPlayerData != null && CollisionPlayerData != this && (CollisionPlayerData.AttackTimer > 0.2f && CollisionPlayerData.attacked))
+            {
+                CollisionPlayerData.isParried = true;
+                CollisionPlayerData.attacked = false;
 
+                swordCollider.enabled = false;
+                parried = false;
+                ParryTimer = 0.5f;
+            }
         }
     }
 }
