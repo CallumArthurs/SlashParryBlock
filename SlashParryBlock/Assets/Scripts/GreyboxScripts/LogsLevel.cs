c3c91a4873107c40cc5abe0ad153ac144f8b8a7e
joshjacobs73 <josh.jacobs@outlook.com>
2019-09-06 16:50:17 +1000

    SpikeyHills, Logs Level fail, Made new scene, edited spikes prefab

---
+++ SlashParryBlock/Assets/Scripts/GreyboxScripts/LogsLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogsLevel : MonoBehaviour { 
+
+    public GameObject Log1;
+    public GameObject Log2;
+    public GameObject Log3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Log1.transform.Rotate(Vector3.up, 20 * Time.deltaTime);
+        //Log1.GetComponent<Rigidbody>().MoveRotation(Quaternion.RotateTowards(Log1.GetComponent<Rigidbody>().rotation, Log1.GetComponent<Rigidbody>().rotation += 20));
+
+    }
+}
