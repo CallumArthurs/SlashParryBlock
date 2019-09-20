d7c025d6061c75ab8b951ce0205582754a8c2ba1
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2019-09-20 14:18:54 +1000

    Update SpinSomething.cs

--- SlashParryBlock/Assets/Scripts/SpinSomething.cs
+++ SlashParryBlock/Assets/Scripts/SpinSomething.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class SpinSomething : MonoBehaviour
 {
+    public enum Axis
+    {
+        XAxis,
+        YAxis,
+        ZAxis
+    }
+    public Axis axis;
     public float speed = 1.0f;
 
     void Update()
     {
-        transform.Rotate(Vector3.up, speed, Space.World);
+        switch ((int)axis)
+        {
+            case 0:
+                transform.Rotate(Vector3.right, speed, Space.World);
+                break;
+            case 1:
+                transform.Rotate(Vector3.up, speed, Space.World);
+                break;
+            case 2:
+                transform.Rotate(Vector3.forward, speed, Space.World);
+                break;
+        }
+        //transform.Rotate(Vector3.up, speed, Space.World);
     }
 }
