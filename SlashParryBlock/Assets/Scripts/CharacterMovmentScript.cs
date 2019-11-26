ead51ab9884ffbaabddb5861a481fb769486d08b
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2019-11-26 11:55:48 +1100

    New Knights Implmented into every map

--- SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
+++ SlashParryBlock/Assets/Scripts/CharacterMovmentScript.cs
@@ -128,8 +128,11 @@ public class CharacterMovmentScript : MonoBehaviour
             players[j].heartFollower = heartFollowers[levelData.meshSelected[j]].gameObject;
             PlayerRenderTextures.Add(Resources.Load("Prefabs/" + joystickCharInputs[j] + "TargetTexture") as RenderTexture);
             PlayerCams[j].GetComponent<Camera>().targetTexture = PlayerRenderTextures[j];
-            Instantiate(levelData.KnightSwords[levelData.meshSelected[j]], players[j].SwordPos.transform);
-            Instantiate(levelData.KnightShields[levelData.meshSelected[j]], players[j].ShieldPos.transform);
+            if (levelData.meshSelected[j] > 3)
+            {
+                Instantiate(levelData.KnightSwords[0], players[j].SwordPos.transform);
+                Instantiate(levelData.KnightShields[0], players[j].ShieldPos.transform);
+            }
 
             List<SkinnedMeshRenderer> skinnedMeshRenderers = new List<SkinnedMeshRenderer>();
             skinnedMeshRenderers.AddRange(players[j].GetComponentsInChildren<SkinnedMeshRenderer>());
--- SlashParryBlock/Assets/Scripts/levelLoadInfo.cs
+++ SlashParryBlock/Assets/Scripts/levelLoadInfo.cs
@@ -30,6 +30,7 @@ public class levelLoadInfo : MonoBehaviour
                 tmpSword = Resources.Load("Prefabs/p_Sword_" + i + "_Spawn") as GameObject;
                 if (tmpSword == null)
                 {
+                    tmpSword = Resources.Load("Prefabs/p_Sword_" + "1" + "_Spawn") as GameObject;
                     break;
                 }
 
@@ -43,6 +44,7 @@ public class levelLoadInfo : MonoBehaviour
                 tmpShield = Resources.Load("Prefabs/p_Shield_" + i + "_Spawn") as GameObject;
                 if (tmpShield == null)
                 {
+                    tmpShield = Resources.Load("Prefabs/p_Shield_" + "1" + "_Spawn") as GameObject;
                     break;
                 }
 
@@ -62,6 +64,7 @@ public class levelLoadInfo : MonoBehaviour
             tmpSword = Resources.Load("Prefabs/p_Sword_" + i + "_Spawn") as GameObject;
             if (tmpSword == null)
             {
+                tmpSword = Resources.Load("Prefabs/p_Sword_" + "1" + "_Spawn") as GameObject;
                 break;
             }
 
@@ -75,6 +78,7 @@ public class levelLoadInfo : MonoBehaviour
             tmpShield = Resources.Load("Prefabs/p_Shield_" + i + "_Spawn") as GameObject;
             if (tmpShield == null)
             {
+                tmpShield = Resources.Load("Prefabs/p_Shield_" + "1" + "_Spawn") as GameObject;
                 break;
             }
 
